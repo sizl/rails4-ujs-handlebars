b98dbb8523555044dbd17e27a3b0de4e3883b8ad
Kevin Liu <kvnliu@gmail.com>
2013-08-08 02:31:35 -0400

    Initial commit -rails only

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NmrRails::Application.config.secret_key_base = '5550982d40ff537fd7551fcbd827f9585fa255ebb71ba611e8714c1e4077c0f288fa5a1ddf6f639b0f430c830057912c3c1c9652afb9a58be4f694943d194ee7'
